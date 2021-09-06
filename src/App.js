c069a6a198d7e81a0f84e7d311e4a5790cdb324a
unknown <jruanova1987@gmail.com>
2021-09-06 19:01:10 -0300

    First commit react

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import "./App.css";
 import WebLayuot from "./_layout/WebLayout";
 import Routes from "./_components/Routes";
 import { Background } from "./_components/Styles";
-import { LightTheme, DarkTheme } from "../src/_layout/Themes";
+
 
 
 function App() {
   return (
-    <ThemeProvider theme={LightTheme}>
+    
       <Background>
         <WebLayuot>
           <Routes />
         </WebLayuot>
       </Background>
-    </ThemeProvider>
+    
   );
 }
 
